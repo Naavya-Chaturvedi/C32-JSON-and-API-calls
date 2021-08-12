de8f87dbd9dd27f038c99b459a5a0cf2e6a8e239
Naavya-Chaturvedi <82451906+Naavya-Chaturvedi@users.noreply.github.com>
2021-08-12 13:53:13 +0530

    Add files via upload

---
+++ jsonEg.js
@@ -0,0 +1,17 @@
+
+
+var student = ["Naavya", 10, "NASA", "black", true];
+console.log(student[3])
+
+var student1 = {
+    "name": "Naavya",
+    "grade": 10,
+    "passion": "NASA",
+    "color": "black",
+    "pet": true
+}
+
+console.log(student1.passion);
+
+name1 = "_N+a-avya";
+console.log(name1[0])
